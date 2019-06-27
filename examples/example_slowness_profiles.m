b35f41647bb4fbddb38a5267cd6ec968eb4cff0a
Danny Rama <50415847+dannyramasawmy@users.noreply.github.com>
2019-06-27 15:44:49 +0100

    Add files via upload
    Updated examples

--- examples/example_slowness_profiles.m
+++ examples/example_slowness_profiles.m
@@ -10,7 +10,7 @@
 %   unfamilar with the class attibutes and methods.
 
 % =========================================================================
-%   INITALISE THE MEDIUM
+%   PLOT SLOWNESS CURVES
 % =========================================================================
 % firstly create a Medium object with each material needed to generate a
 % plot. Here the slowness profiles of water, glass and beryl will be
@@ -20,4 +20,82 @@ myMedium = Medium.generateLayeredMedium('water',Inf,'glass',0.001,'beryl2',Inf);
 % calculate the slowness profiles
 myMedium.calculateSlowness;
 
+% the slowness profiles can be plotted, since these are symmetric about the
+% axes, the are only plotted for a single quadrant
 myMedium.plotSlowness;
+
+
+% =========================================================================
+%   MANUAL PLOTTING
+% =========================================================================
+% to manually plot the curves, the kx vs kz data is given in a structure
+% form.
+
+% slowness data for the first material
+slownessDataMat_1 = myMedium(1).slowness;
+
+% slowness data for the second material
+slownessDataMat_2 = myMedium(2).slowness;
+
+% slowness data for the third material
+slownessDataMat_3 = myMedium(3).slowness;
+
+% The .calcualteSlowness function will return complex values under certain
+% conditions. Therefore only the real part should be plotted. 
+% Additionally these may be positive or negative kz -> these are symmetric
+% however
+
+figure;
+
+% plot the (q)-L component (kz_qL1)
+plot( real(slownessDataMat_2.kx), abs(real(slownessDataMat_2.kz_qL1)) ,'k' )
+hold on
+% plot the (q)-SV component (kzt_qSV1)
+plot( real(slownessDataMat_2.kx), abs(real(slownessDataMat_2.kz_qSV1)) ,'b' )
+
+% plot the (q)-SH component (kz_qSH)
+plot( real(slownessDataMat_2.kx), abs(real(slownessDataMat_2.kz_qSH)), 'r--')
+hold off
+% labels
+axis equal
+xlabel('k_x /\omega')
+ylabel('k_z /\omega')
+legend('L','SV','SH')
+title('Manual slowness plot')
+
+
+% =========================================================================
+%   CALCULATE ERRORS
+% =========================================================================
+disp('Errors are within numerical precision : ')
+
+% note the compressional speed of glass is 1/L curve
+cLGlass = 5570; % see materialList
+cLGlassSlowness = 1/ sqrt(real(slownessDataMat_2.kx(2)).^2 + real(slownessDataMat_2.kz_qL1(2)).^2);
+differenceErrorCL = abs((cLGlass - cLGlassSlowness)/5570);
+
+% note the shear (vertical) speed of glass is 1/SV curve
+cSGlass = 3430; % see materialList
+cSGlassSlowness = 1/ sqrt(real(slownessDataMat_2.kx(2)).^2 + real(slownessDataMat_2.kz_qSV1(2)).^2);
+differenceErrorCSV = abs((cSGlass - cSGlassSlowness)/3430);
+
+% note the shear (horizontal) speed of glass is 1/SH = 1/SV curve
+cSGlassSlowness = 1/ sqrt(real(slownessDataMat_2.kx(2)).^2 + real(slownessDataMat_2.kz_qSH(2)).^2);
+differenceErrorCSH = abs((cSGlass - cSGlassSlowness)/3430);
+
+disp(['Error in cL   :', num2str(differenceErrorCL) ])
+disp(['Error in cSV  :', num2str(differenceErrorCSV)])
+disp(['Error in cSH  :', num2str(differenceErrorCSH)])
+
+
+
+
+
+
+
+
+
+
+
+
+
