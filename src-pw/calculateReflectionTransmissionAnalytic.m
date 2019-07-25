3cab2342b70c47094a7100769c7afc02cb06c71b
Danny Rama <50415847+dannyramasawmy@users.noreply.github.com>
2019-07-25 14:02:05 +0100

    Add files via upload
    Updated documentation to calculateReflectionTransmissionAnalytic

--- src-pw/calculateReflectionTransmissionAnalytic.m
+++ src-pw/calculateReflectionTransmissionAnalytic.m
@@ -1,101 +1,178 @@
-function [RL, RS, TL, TS] = calculateReflectionTransmissionAnalytic(angleRange, mediumObject)
-    % calculateReflectionTransmissionAnalytic
+function [RL, RS, TL, TS] = calculateReflectionTransmissionAnalytic(...
+        angle_range, medium_object)
+    %CALCULATEREFLECTIONTRANSMISSIONANALYTIC analytic R and T coefficients.
     %
-    % Author    :   Danny Ramasawmy
-    %               rmapdrr@ucl.ac.uk dannyramasawmy@gmail.com
-    % Date      :   2019-07-05  -   created
+    % DESCRIPTION
+    %   CALCULATEREFLECTIONTRANSMISSIONANALYTIC calculates the
+    %   angle-dependent reflection and transmission (displacement)
+    %   coefficients between two half-spaces using the expressions given in
+    %   [1], and [2].
     %
-    % This function calculates the analytical reflection and transmission
-    % coefficients between two half-spaces using the expressions given in
-    % [1], and [2].
+    %   [1] Rose, Joseph L. Ultrasonic guided waves in solid media.
+    %       Cambridge university press, 2014.
+    %   [2] Cheeke, J. David N. Fundamentals and applications of ultrasonic
+    %       waves. CRC press, 2016.
     %
-    % [1] Rose, Joseph L. Ultrasonic guided waves in solid media.
-    %   Cambridge university press, 2014.
-    % [2] Cheeke, J. David N. Fundamentals and applications of ultrasonic
-    %   waves. CRC press, 2016.
-
+    % USEAGE
+    %   [RL, RS, TL, TS] = calculateReflectionTransmissionAnalytic(...
+    %       angle_range, medium_object);
     %
-    % =====================================================================
-    %   SORT MATERIAL PROPERTIES
-    % =====================================================================
+    % INPUTS
+    %   angle_range     - a range of input angles               [degrees]
+    %   medium_object   - a medium object with two isotropic layers
+    %
+    % OPTIONAL INPUTS
+    %   []              - there are no optional inputs              []
+    %
+    % OUTPUTS
+    %   RL              - reflected compressional coefficient       []
+    %   RS              - reflected shear coefficient               []
+    %   TL              - transmitted compressional coefficient     []
+    %   TS              - transmitted shear coefficient             []
+    %
+    % DEPENDENCIES
+    %   Medium class    - requires a Medium class object
+    %
+    % ABOUT
+    %   author          - Danny Ramasawmy
+    %   contact         - dannyramasawmy+elasticmatrix@gmail.com
+    %   date            - 05 - July     - 2019
+    %   last update     - 25 - July     - 2019
+    %
+    % This file is part of the ElasticMatrix toolbox.
+    % Copyright (c) 2019 Danny Ramasawmy.
+    %
+    % This file is part of ElasticMatrix. ElasticMatrix is free software:
+    % you can redistribute it and/or modify it under the terms of the GNU
+    % Lesser General Public License as published by the Free Software
+    % Foundation, either version 3 of the License, or (at your option) any
+    % later version.
+    %
+    % ElasticMatrix is distributed in the hope that it will be useful, but
+    % WITHOUT ANY WARRANTY; without even the implied warranty of
+    % MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    % Lesser General Public License for more details.
+    %
+    % You should have received a copy of the GNU Lesser General Public
+    % License along with ElasticMatrix. If not, see
+    % <http://www.gnu.org/licenses/>.
     
-    % check material properties follow a Medium structure
-    try
-        if class(mediumObject) == 'Medium'
-            % check it has a length of 2
-            if length(mediumObject) ~= 2
-                warning('Use a Medium object of length 2')
-                return
-            end
-            
-            % check the layers are not anisotropic
-            if sum(strcmp({mediumObject(1).state, mediumObject(2).state},'Anisotropic')) > 0
-                warning('This function is not valid for anisotropic materials')
-            end
-            
-        end
-    catch
-        % not used a medium object
-        warning('Incorrect input arguments')
-        return;
-    end
+    % check inputs
+    inputCheck(angle_range, medium_object);
+    
+    % =====================================================================
+    %   SORT MATERIAL PROPERTIES AND CALCULATE
+    % =====================================================================
     
     % reassign data density
-    rho1    = mediumObject(1).density;
-    rho2    = mediumObject(2).density;
+    rho1    = medium_object(1).density;
+    rho2    = medium_object(2).density;
     % compressional speed
-    cL1     = sqrt(mediumObject(1).stiffness_matrix(1,1) ./ rho1);
-    cL2     = sqrt(mediumObject(2).stiffness_matrix(1,1) ./ rho2);
+    cL1     = sqrt(medium_object(1).stiffness_matrix(1,1) ./ rho1);
+    cL2     = sqrt(medium_object(2).stiffness_matrix(1,1) ./ rho2);
     % shear speed
-    cS1     = sqrt(mediumObject(1).stiffness_matrix(5,5) ./ rho1);
-    cS2     = sqrt(mediumObject(2).stiffness_matrix(5,5) ./ rho2);
+    cS1     = sqrt(medium_object(1).stiffness_matrix(5,5) ./ rho1);
+    cS2     = sqrt(medium_object(2).stiffness_matrix(5,5) ./ rho2);
     
     % choose calculation option
-    switch mediumObject(1).state
+    switch medium_object(1).state
         case {'Liquid'}
             % use Cheeke solution
-            [RL, RS, TL, TS] = rTSolutionCheeke(cL1, cL2, cS2, rho1, rho2, angleRange);
-
+            [RL, RS, TL, TS] = rTSolutionCheeke(...
+                cL1, cL2, cS2, rho1, rho2, angle_range);
+            
         otherwise
-            % use Rose soution
-            [RL, RS, TL, TS] = rTSolutionRose(cL1, cL2, cS1, cS2, rho1, rho2, angleRange);
+            % use Rose solution
+            [RL, RS, TL, TS] = rTSolutionRose(...
+                cL1, cL2, cS1, cS2, rho1, rho2, angle_range);
     end
     
     
     
 end
 
+function inputCheck(angle_range, medium_object)
+    %INPUECHECK Checks the inputs for the current function.
+    %
+    % DESCRIPTION
+    %   INPUTCHECK(angle_range) checks the inputs for the function
+    %   calculateReflectionTransmissionAnalytic(...). If any of the inputs
+    %   are not valid, the function will break and print errors to screen.
+    %
+    % USAGE
+    %   inputChecks(angle_range);
+    %
+    % ABOUT
+    %   author          - Danny Ramasawmy
+    %   contact         - dannyramasawmy+elasticmatrix@gmail.com
+    %   date            - 20 - July - 2019
+    %   last update     - 25 - July - 2019
+    
+    % define attributes
+    attributes = {'real'};
+    
+    % validate the attributes for input 1
+    validateattributes(angle_range, {'numeric'}, attributes,...
+        'calculateReflectionTran...', 'angle_range', 1);
+    
+    % check material properties are a Medium class
+    if isa(medium_object, 'Medium')
+        % check it has a length of 2
+        if length(medium_object) ~= 2
+            error('Use a Medium object of length 2.')
+        end
+        
+        % check the layers are not anisotropic
+        if sum(strcmp({medium_object(1).state, medium_object(2).state},'Anisotropic')) > 0
+            error('This function is not valid for anisotropic materials')
+        end
+    else
+        % not used a medium object
+        warning('Incorrect input arguments')
+    end
+    
+end
 
-function  [RL, RS, TL, TS] = rTSolutionRose(cL1, cL2, cT1, cT2, rho1, rho2, angleRange)
-    % rTSolutionRose
+function  [RL, RS, TL, TS] = rTSolutionRose(...
+        cL1, cL2, cT1, cT2, rho1, rho2, angle_range)
+    %RTSOULUTIONROSE analytic R and T coefficients.
     %
-    % Author    :   Danny Ramasawmy
-    %               rmapdrr@ucl.ac.uk dannyramasawmy@gmail.com
-    % Date      :   2019-07-05  -   created
+    % DESCRIPTION
+    %   RTSOULUTIONROSE calculates the
+    %   angle-dependent reflection and transmission (displacement)
+    %   coefficients between two half-spaces using the expressions given in
+    %   [1].
     %
-    % This function calcualtes the R and T coefficients between two half
-    % spacaes which both have elastic properties no reflected shear
+    %   [1] Rose, Joseph L. Ultrasonic guided waves in solid media.
+    %       Cambridge university press, 2014.
     %
-    % [1] Rose, Joseph L. Ultrasonic guided waves in solid media.
-    %   Cambridge university press, 2014.
+    % USEAGE
+    %   [RL, RS, TL, TS] = rTSolutionRose(...
+    %       cL1, cL2, cT1, cT2, rho1, rho2, angle_range);
+    %
+    % ABOUT
+    %   author          - Danny Ramasawmy
+    %   contact         - dannyramasawmy+elasticmatrix@gmail.com
+    %   date            - 05 - July     - 2019
+    %   last update     - 25 - July     - 2019
     
     % initalise coefficient matrices
-    RL = zeros(size(angleRange));
-    RS = zeros(size(angleRange));
-    TL = zeros(size(angleRange));
-    TS = zeros(size(angleRange));
+    RL = zeros(size(angle_range));
+    RS = zeros(size(angle_range));
+    TL = zeros(size(angle_range));
+    TS = zeros(size(angle_range));
     
     % incident layer wave velocity
     cN = cL1;
     
     % loop over angles
-    for idx = 1:length(angleRange)
+    for idx = 1:length(angle_range)
         % incidence angle
-        theta = angleRange(idx) * pi /180;
+        theta = angle_range(idx) * pi /180;
         omega = 2 * pi * 1e6;
         
         
-        % angle of incidence/ reflectoin
+        % angle of incidence/ reflection
         alphaL = asin( sin(theta) * cL1 / cN );
         alphaT = asin( sin(theta) * cT1 / cN );
         
@@ -103,16 +180,16 @@ function  [RL, RS, TL, TS] = rTSolutionRose(cL1, cL2, cT1, cT2, rho1, rho2, angl
         betaT = asin( sin(theta) * cT2 / cN );
         
         % calculate wave-vectors horizontal component
-        kx = (omega / cN) * sin(theta);
+        % kx = (omega / cN) * sin(theta);
         
-        % compresional wavevector
+        % compressional wave-vector
         kL1 = (omega / cL1) ;
         kL2 = (omega / cL2) ;
-        % shear wavevectors
+        % shear wave-vectors
         kT1 = (omega / cT1) ;
         kT2 = (omega / cT2) ;
         
-        % defin lame parameters
+        % define lame parameters
         mu1        = rho1 * (cT1^2);
         lambda1    = rho1 * (cL1^2) - 2*mu1;
         
@@ -124,84 +201,92 @@ function  [RL, RS, TL, TS] = rTSolutionRose(cL1, cL2, cT1, cT2, rho1, rho2, angl
         factor2 = kL2 * (lambda2 + 2*mu2);
         
         % input vector
-        incidentVector = [...
+        incident_vector = [...
             -cos(alphaL);
             sin(alphaL);
             factor1 * cos(2*alphaT);
             -kL1 * mu1 * sin(2 * alphaL)];
         
         % system matrix
-        systemMatrix = [...
+        system_matrix = [...
             -cos(alphaL)               ,  sin(alphaT)                 , -cos(betaL)              , sin(betaT);
             -sin(alphaL)               , -cos(alphaT)                 , sin(betaL)               , cos(betaT);
             -factor1 * cos(2*alphaT)  ,  kT1 * mu1 * sin(2*alphaT) , factor2 * cos(2*betaT)  , -kT2 * mu2 * sin(2*betaT);
             -kL1*mu1*sin(2*alphaL)   , -kT1*mu1*cos(2*alphaT)     , -kL2*mu2*sin(2*betaL)  , -kT2 * mu2 * cos(2 * betaT)];
         
         % Solve with inverse
-        partialWaveAmps = systemMatrix \ incidentVector;
+        partial_wave_amps = system_matrix \ incident_vector;
         
-        % the soundspeed/soundspeed converts the velocity coefficients to
+        % the sound-speed_1/sound-speed_2 converts the velocity coefficients to
         % displacement coefficients
-        RL(idx) = partialWaveAmps(1) * cL1/cL1 ;
-        RS(idx) = partialWaveAmps(2) * cT1/cL1 ;
-        TL(idx) = partialWaveAmps(3) * cL2/cL1 ;
-        TS(idx) = partialWaveAmps(4) * cT2/cL1 ;
+        RL(idx) = partial_wave_amps(1) * cL1/cL1 ;
+        RS(idx) = partial_wave_amps(2) * cT1/cL1 ;
+        TL(idx) = partial_wave_amps(3) * cL2/cL1 ;
+        TS(idx) = partial_wave_amps(4) * cT2/cL1 ;
     end
     
 end
 
 
 
-function  [RL, RS, TL, TS] = rTSolutionCheeke(cL1, cL2, cS2, rho1, rho2, angleRange)
-    % rTSolutionCheeke
+function  [RL, RS, TL, TS] = rTSolutionCheeke(...
+        cL1, cL2, cS2, rho1, rho2, angle_range)
+    %RTSOULUTIONCHEEKE analytic R and T coefficients.
     %
-    
-    % Author    :   Danny Ramasawmy
-    %               rmapdrr@ucl.ac.uk dannyramasawmy@gmail.com
-    % Date      :   2019-07-05  -   created
+    % DESCRIPTION
+    %   RTSOULUTIONCHEEKE calculates the
+    %   angle-dependent reflection and transmission (displacement)
+    %   coefficients between two half-spaces using the expressions given in
+    %   [1].
     %
-    % This function calcualtes the R and T coefficients between two half
-    % spacaes where the first layer has fluid properties and the second
-    % layer has elastic properties
+    %   [1] Cheeke, J. David N. Fundamentals and applications of ultrasonic
+    %       waves. CRC press, 2016.
     %
-    %   solution is from [1] [1] Cheeke, J. David N. Fundamentals and
-    %   applications of ultrasonic waves. CRC press, 2016.
+    % USEAGE
+    %   [RL, RS, TL, TS] = rTSolutionCheeke(...
+    %       cL1, cL2, cS2, rho1, rho2, angle_range);
+    %
+    % ABOUT
+    %   author          - Danny Ramasawmy
+    %   contact         - dannyramasawmy+elasticmatrix@gmail.com
+    %   date            - 05 - July     - 2019
+    %   last update     - 25 - July     - 2019
     %
     
     % initalise coefficient matrices
-    RL = zeros(size(angleRange));
-    RS = zeros(size(angleRange));
-    TL = zeros(size(angleRange));
-    TS = zeros(size(angleRange));
+    RL = zeros(size(angle_range));
+    RS = zeros(size(angle_range));
+    TL = zeros(size(angle_range));
+    TS = zeros(size(angle_range));
     
     % loop over angles
-    for idx = 1:length(angleRange)
+    for idx = 1:length(angle_range)
         
         % angle in radians
-        theta =  angleRange(idx) * pi / 180 ;
+        theta =  angle_range(idx) * pi / 180 ;
         
-        % Snells law - angles of refraction
-        thetaL = asin( sin(theta)*(cL2)/(cL1)  );
-        thetaS = asin( sin(theta)*(cS2)/(cL1)  );
+        % Snell's law - angles of refraction
+        theta_L = asin( sin(theta)*(cL2)/(cL1)  );
+        theta_S = asin( sin(theta)*(cS2)/(cL1)  );
         
-        % Impeadances
+        % Impedances
         Z1 = rho1 * cL1 / cos(theta);
-        ZL = rho2 * cL2 / cos(thetaL);
-        ZS = rho2 * cS2 / cos(thetaS);
+        ZL = rho2 * cL2 / cos(theta_L);
+        ZS = rho2 * cS2 / cos(theta_S);
         
         % effective impedance (for each angle)
-        Zeff = ZL*(cos(2*thetaS))^2 + ZS*(sin(2*thetaS))^2;
+        Zeff = ZL*(cos(2*theta_S))^2 + ZS*(sin(2*theta_S))^2;
         
         % reflected compressional wave
         RL(idx) = (Zeff - Z1) / (Zeff + Z1);
         
         % transmitted compressional wave
         TL(idx) = (rho1 / rho2)*...
-            (2*ZL*cos(2*thetaS))/(Zeff+Z1);
+            (2*ZL*cos(2*theta_S))/(Zeff+Z1);
         
         % transmitted shear wave
         TS(idx) = -(rho1 /rho2)*...
-            (2*ZS*sin(2*thetaS))/(Zeff+Z1);
+            (2*ZS*sin(2*theta_S))/(Zeff+Z1);
     end
     
     
